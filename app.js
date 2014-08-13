1cb6f4b510a27243ebe019383382c30a83b07537
rueda10 <rueda10@users.noreply.github.com>
2014-08-13 17:22:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,38 @@ var main = function() {
     currentDot.removeClass('active-dot');
     prevDot.addClass('active-dot');
   });
+  
+  $(document).keypress(function(event) {
+    if (event.which == 37) {
+      var currentSlide = $('.active-slide');
+      var prevSlide = currentSlide.prev();
+      var currentDot = $('.active-dot');
+      var prevDot = currentDot.prev();
+    
+      if (prevSlide.length == 0) {
+          prevSlide = $('.slide').last();
+          prevDot = $('.dot').last() 
+      }
+      currentSlide.fadeOut(600).removeClass('active-slide');
+      prevSlide.fadeIn(600).addClass('active-slide');
+      currentDot.removeClass('active-dot');
+      prevDot.addClass('active-dot');
+    } else if (event.which == 39) {
+      var currentSlide = $('.active-slide');
+      var nextSlide = currentSlide.next();
+      var currentDot = $('.active-dot');
+      var nextDot = currentDot.next();
+    
+      if (nextSlide.length == 0) {
+          nextSlide = $('.slide').first();
+          nextDot = $('.dot').first();
+      }
+      currentSlide.fadeOut(600).removeClass('active-slide');
+      nextSlide.fadeIn(600).addClass('active-slide');
+      currentDot.removeClass('active-dot');
+      nextDot.addClass('active-dot');
+    }
+  });
 }
 
 $(document).ready(main);
